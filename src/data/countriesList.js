fe6d6b68a2c05d5b86fb803a7a01be81b1f2edc9
Sebastián Fretes <sebafretesca@gmail.com>
2024-09-02 19:27:45 -0400

    countriesList.js created

---
+++ src/data/countriesList.js
@@ -0,0 +1,9 @@
+export const countries = [
+    { code: 'US', name: 'Estados Unidos' },
+    { code: 'PY', name: 'Paraguay' },
+    { code: 'UY', name: 'Uruguay' },
+    { code: 'AR', name: 'Argentina' },
+    { code: 'CO', name: 'Colombia' },
+    { code: 'BR', name: 'Brasil' },
+    { code: 'ES', name: 'España' },
+  ]
