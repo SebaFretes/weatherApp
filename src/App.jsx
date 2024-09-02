3bc53a524cea3be9c0a78cc2b243a3481349ca6e
Sebastián Fretes <sebafretesca@gmail.com>
2024-09-02 18:46:54 -0400

    App.modules.css created and styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import styles from './App.module.css';
 
 export const App = () => {
 
   return (
     <>
-      <h1>Weather App</h1>
+      <h1 className={styles.title}>Weather App</h1>
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
