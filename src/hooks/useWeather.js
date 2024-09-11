6f6e3906cd482bffb4742c6bdcd13996774a1c18
Sebastián Fretes <sebafretesca@gmail.com>
2024-09-11 17:07:59 -0400

    Test that API data works and useState added to manage weather state

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,24 +1,46 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 export const useWeather = () => {
 
-    const fetchWeather = async(search) => {
+  const [weather, setWeather] = useState({
+    name: '',
+    temp: 0,
+    humidity: 0
+  })
 
-      const apiKey = import.meta.env.VITE_API_KEY
-        
-        try {
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`;
-            const {data} = await axios(url);
-            console.log(data);
-            
-            
-        } catch (error) {
-            
-        }
-        
+  const fetchWeather = async (search) => {
+
+    const apiKey = import.meta.env.VITE_API_KEY
+
+    try {
+      const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`;
+      const { data } = await axios(geoURL);
+
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+      const { data: result } = await axios(weatherURL);
+
+      setWeather({
+        name: result.name,
+        temp: Math.round(result.main.temp),
+        humidity: result.main.humidity
+      });
+
+      console.log(result.name);
+      console.log(`Temperatura ${Math.round(result.main.temp)}°C`);
+      console.log(`Humedad ${result.main.humidity}%`);
+
+    } catch (error) {
+      console.error("Error fetching weather data:", error.message);
     }
+  }
 
   return {
+    weather,
     fetchWeather
   }
 }
