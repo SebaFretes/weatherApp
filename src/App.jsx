b4988d6464592be1280120535f18f89e09323a49
Sebastián Fretes <sebafretesca@gmail.com>
2024-09-11 17:09:02 -0400

    weather imported in useWeather hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useWeather } from './hooks/useWeather';
 
 export const App = () => {
 
-  const { fetchWeather } = useWeather();  
+  const { weather, fetchWeather } = useWeather();  
 
   return (
     <>
